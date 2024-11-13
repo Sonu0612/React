3b6e4f293466bbaa8d0e8e0da92d0bbb27f17963
sonu <sohanmanjunath496@gmail.com>
2024-11-13 19:31:55 +0530

    Initial commit5

--- App.js
+++ App.js
@@ -1,3 +1,8 @@
-const heading = React.createElement("h1",{id:"heading"},"Hello World!");
+import React from "react";
+import ReactDOM from "react-dom";
+const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},
+    [React.createElement("h1",{},"learning react🫵")]
+))
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading);
+root.render(parent);
+
