b8ec856e6194527c98b5fdb79129197a6181ebcd
Sonu <166245586+Sonu0612@users.noreply.github.com>
2024-11-17 10:17:14 +0530

    Delete dist directory

--- dist/index.6bd02f5a.js
+++
@@ -1,7 +0,0 @@
-const heading = React.createElement("h1", {
-    id: "heading"
-}, "Hello World!");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
-//# sourceMappingURL=index.6bd02f5a.js.map
