1b2021d14ca290fdabb6135aab8183e733b3cf1e
Sonu <166245586+Sonu0612@users.noreply.github.com>
2024-11-17 10:11:36 +0530

    Delete App.js

--- App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},
-    [React.createElement("h1",{},"learning react🫵")]
-))
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent);
-
